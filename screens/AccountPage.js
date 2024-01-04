20bce38a01c141e8b18cf4a922879c791391af35
Siya(Xiran) Yan <siyayan1222@gmail.com>
2024-01-04 20:12:43 +1100

    fix issues

--- screens/AccountPage.js
+++ screens/AccountPage.js
@@ -82,6 +82,12 @@ const AccountScreen = ({ navigation }) => {
 
     if (!result.canceled) {
       setSelectedImage(result);
+      console.log('Take Image Result:', result);
+      updateUserData({
+        token: userData.token,
+        userName: userData.userName,
+        avatar: result.assets[0].uri
+      })
       // console.log('Take Image Result:', result);
       handleUploadImage();
     }
