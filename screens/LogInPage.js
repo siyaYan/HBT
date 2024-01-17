205a5bdf8ddba69d50dfdeef5caed685d32117b8
Siya(Xiran) Yan <siyayan1222@gmail.com>
2024-01-17 22:31:59 +1100

    fix: display background issue

--- screens/LogInPage.js
+++ screens/LogInPage.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import {
   VStack,
   HStack,
+  ZStack,
   FormControl,
   Box,
   Button,
@@ -140,192 +141,208 @@ const LoginScreen = ({ navigation }) => {
   };
 
   return (
-    <ImageBackground
-      source={require("../assets/background.png")} // Replace with the actual path to your image
-      style={{ flex: 1, resizeMode: "cover" , opacity:"0.2" }}
-    >
-      <Center w="100%">
-        <Box py="5" safeArea w="100%" h="100%" maxW="300">
-          <Center w="100%" h="100%">
-            <VStack w="100%" space={8}>
-              <HStack
-                w="95%"
-                style={{ alignSelf: "center", justifyContent: "space-between" }}
-              >
-                <Text fontSize="4xl" style={{ fontFamily: "Bold" }}>
-                  Log in
-                </Text>
-                <VStack>
-                  <Text fontFamily={"Regular"} fontSize="lg">
-                    New to Habital?{"  "}
-                  </Text>
-                  <HStack>
-                    <Pressable
-                      onPress={() =>
-                        navigation.navigate("LoginStack", {
-                          screen: "Register",
-                        })
-                      }
-                    >
-                      <Text
-                        fontFamily={"Regular Semi Bold"}
-                        fontSize="lg"
-                        color="#49a579"
-                      >
-                        Sign Up
-                      </Text>
-                    </Pressable>
-                    <Text fontFamily={"Regular"} fontSize="lg">
-                      {" "}
-                      to start!
-                    </Text>
-                  </HStack>
-                </VStack>
-              </HStack>
 
-              <VStack space={7} mt="5">
-                <Button
-                  rounded={30}
-                  shadow="6"
-                  size="lg"
-                  onPress={() => signIn()}
-                >
-                  Google
-                </Button>
-                <Button
-                  rounded={30}
-                  shadow="6"
-                  size="lg"
-                  onPress={() => signIn()}
-                >
-                  Facebook
-                </Button>
-                <Text
-                  textAlign={"center"}
-                  fontFamily={"Regular Semi Bold"}
-                  fontSize={"lg"}
-                >
-                  Or
-                </Text>
+    <Center w="100%">
+                  <Box
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            opacity={0.2} // Set the opacity to 0.2 for 80% transparency
+          >
+            <Image
+              source={require("../assets/background.png")}
+              style={{ width: "100%", height: "100%" }}
+              alt="image"
+            />
+          </Box>
+      <Box
+        justifyContent="center"
+        alignItems="center"
+        position="relative"
+        safeArea
+        h="100%"
+        maxW="80%"
+      >
+        <Center w="90%" h="100%">
 
-                <FormControl isRequired isInvalid={"id" in errors}>
-                  <Input
-                    rounded={30}
-                    size="2xl"
-                    fontFamily={"Regular"}
-                    onChangeText={(value) =>
-                      setData({
-                        ...formData,
-                        id: value,
+          <VStack w="100%" space={8}>
+            <HStack
+              w="95%"
+              style={{ alignSelf: "center", justifyContent: "space-between" }}
+            >
+              <Text fontSize="4xl" style={{ fontFamily: "Bold" }}>
+                Log in
+              </Text>
+              <VStack>
+                <Text fontFamily={"Regular"} fontSize="lg">
+                  New to Habital?{"  "}
+                </Text>
+                <HStack>
+                  <Pressable
+                    onPress={() =>
+                      navigation.navigate("LoginStack", {
+                        screen: "Register",
                       })
                     }
-                    placeholder="Email/Username"
-                  />
-                  <FormControl.ErrorMessage></FormControl.ErrorMessage>
-                </FormControl>
-
-                <FormControl isRequired isInvalid={"password" in errors}>
-                  <Input
-                    rounded={30}
-                    size="2xl"
-                    fontFamily={"Regular"}
-                    placeholder="Password"
-                    onChangeText={(value) =>
-                      setData({
-                        ...formData,
-                        password: value,
-                      })
-                    }
-                    type={show ? "text" : "password"}
-                    InputRightElement={
-                      <Pressable onPress={() => setShow(!show)}>
-                        <Icon
-                          as={
-                            <MaterialIcons
-                              name={show ? "visibility" : "visibility-off"}
-                            />
-                          }
-                          size={5}
-                          mr="2"
-                          color="muted.400"
-                        />
-                      </Pressable>
-                    }
-                  />
-                </FormControl>
-              </VStack>
-
-              <HStack style={{ justifyContent: "space-between" }}>
-                <VStack space={2}>
-                  <Checkbox
-                    // backgroundColor={"blue"}
-                    colorScheme="green"
-                    ml="1"
-                    size="sm"
-                    defaultIsChecked
-                    onPress={(value) => setRemember(!remember)}
                   >
-                    <Text fontFamily={"Regular"} fontSize={"lg"}>
-                      Remember
-                    </Text>
-                  </Checkbox>
-
-                  <Link>
-                    <Pressable onPress={() => setShowModal(true)}>
-                      <Text
-                        marginLeft={1}
-                        fontFamily={"Regular"}
-                        fontSize={"lg"}
-                        color="#191919"
-                        textDecorationLine={"underline"}
-                      >
-                        Oh no! I forget😱
-                      </Text>
-                    </Pressable>
-                    <Modal
-                      isOpen={showModal}
-                      onClose={() => setShowModal(false)}
+                    <Text
+                      fontFamily={"Regular Semi Bold"}
+                      fontSize="lg"
+                      color="#49a579"
                     >
-                      <Modal.Content maxWidth="400px">
-                        <Modal.CloseButton />
-                        <Modal.Header>Send Reset Password Email</Modal.Header>
-                        <Modal.Body>
-                          <FormControl mt="3" isInvalid={!!error} isRequired>
-                            <FormControl.Label>Email</FormControl.Label>
-                            <Input
-                              value={email}
-                              onChangeText={setEmail}
-                              placeholder="example@email.com"
-                            />
-                            {error ? (
-                              <FormControl.ErrorMessage>
-                                Please enter a valid email address.
-                              </FormControl.ErrorMessage>
-                            ) : (
-                              <FormControl.HelperText></FormControl.HelperText>
-                            )}
-                          </FormControl>
-                        </Modal.Body>
-                        <Modal.Footer>
-                          <Button.Group space={2}>
-                            <Button
-                              variant="ghost"
-                              colorScheme="blueGray"
-                              onPress={() => {
-                                setShowModal(false);
-                                setError(false); // Clear any errors
-                              }}
-                            >
-                              Cancel
-                            </Button>
-                            <Button onPress={handleResend}>Send</Button>
-                          </Button.Group>
-                        </Modal.Footer>
-                      </Modal.Content>
-                    </Modal>
-                  </Link>
-                </VStack>
-                {/* <Button
+                      Sign Up
+                    </Text>
+                  </Pressable>
+                  <Text fontFamily={"Regular"} fontSize="lg">
+                    {" "}
+                    to start!
+                  </Text>
+                </HStack>
+              </VStack>
+            </HStack>
+
+            <VStack space={7} mt="5">
+              <Button
+                rounded={30}
+                shadow="6"
+                size="lg"
+                onPress={() => signIn()}
+              >
+                Google
+              </Button>
+              <Button
+                rounded={30}
+                shadow="6"
+                size="lg"
+                onPress={() => signIn()}
+              >
+                Facebook
+              </Button>
+              <Text
+                textAlign={"center"}
+                fontFamily={"Regular Semi Bold"}
+                fontSize={"lg"}
+              >
+                Or
+              </Text>
+
+              <FormControl isRequired isInvalid={"id" in errors}>
+                <Input
+                  rounded={30}
+                  size="2xl"
+                  fontFamily={"Regular"}
+                  onChangeText={(value) =>
+                    setData({
+                      ...formData,
+                      id: value,
+                    })
+                  }
+                  placeholder="Email/Username"
+                />
+                <FormControl.ErrorMessage></FormControl.ErrorMessage>
+              </FormControl>
+
+              <FormControl isRequired isInvalid={"password" in errors}>
+                <Input
+                  rounded={30}
+                  size="2xl"
+                  fontFamily={"Regular"}
+                  placeholder="Password"
+                  onChangeText={(value) =>
+                    setData({
+                      ...formData,
+                      password: value,
+                    })
+                  }
+                  type={show ? "text" : "password"}
+                  InputRightElement={
+                    <Pressable onPress={() => setShow(!show)}>
+                      <Icon
+                        as={
+                          <MaterialIcons
+                            name={show ? "visibility" : "visibility-off"}
+                          />
+                        }
+                        size={5}
+                        mr="2"
+                        color="muted.400"
+                      />
+                    </Pressable>
+                  }
+                />
+              </FormControl>
+            </VStack>
+
+            <HStack style={{ justifyContent: "space-between" }}>
+              <VStack space={2}>
+                <Checkbox
+                  // backgroundColor={"blue"}
+                  colorScheme="green"
+                  ml="1"
+                  size="sm"
+                  defaultIsChecked
+                  onPress={(value) => setRemember(!remember)}
+                >
+                  <Text fontFamily={"Regular"} fontSize={"lg"}>
+                    Remember
+                  </Text>
+                </Checkbox>
+
+                <Link>
+                  <Pressable onPress={() => setShowModal(true)}>
+                    <Text
+                      marginLeft={1}
+                      fontFamily={"Regular"}
+                      fontSize={"lg"}
+                      color="#191919"
+                      textDecorationLine={"underline"}
+                    >
+                      Oh no! I forget😱
+                    </Text>
+                  </Pressable>
+                  <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+                    <Modal.Content maxWidth="400px">
+                      <Modal.CloseButton />
+                      <Modal.Header>Send Reset Password Email</Modal.Header>
+                      <Modal.Body>
+                        <FormControl mt="3" isInvalid={!!error} isRequired>
+                          <FormControl.Label>Email</FormControl.Label>
+                          <Input
+                            value={email}
+                            onChangeText={setEmail}
+                            placeholder="example@email.com"
+                          />
+                          {error ? (
+                            <FormControl.ErrorMessage>
+                              Please enter a valid email address.
+                            </FormControl.ErrorMessage>
+                          ) : (
+                            <FormControl.HelperText></FormControl.HelperText>
+                          )}
+                        </FormControl>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button.Group space={2}>
+                          <Button
+                            variant="ghost"
+                            colorScheme="blueGray"
+                            onPress={() => {
+                              setShowModal(false);
+                              setError(false); // Clear any errors
+                            }}
+                          >
+                            Cancel
+                          </Button>
+                          <Button onPress={handleResend}>Send</Button>
+                        </Button.Group>
+                      </Modal.Footer>
+                    </Modal.Content>
+                  </Modal>
+                </Link>
+              </VStack>
+              {/* <Button
                   rounded={30}
                   shadow="7"
                   width="50%"
@@ -339,20 +356,19 @@ const LoginScreen = ({ navigation }) => {
                 >
                   Sign in
                  </Button> */}
+              <Pressable onPress={handleSubmit}>
                 <Image
                   source={require("../assets/cherry.png")}
                   style={{ width: 80, height: 80 }}
                   alt="image"
                 />
-                {/* source={{
-                  uri: "../assets/cherry.png",
-                }} */}
-              </HStack>
-            </VStack>
-          </Center>
-        </Box>
-      </Center>
-    </ImageBackground>
+              </Pressable>
+            </HStack>
+          </VStack>
+        </Center>
+      </Box>
+    </Center>
+    // </ImageBackground>
   );
 };
 
