f4227fa2f0b68ddd39ce2673b54807625bdd8ec3
Dan <dan.li@anu.edu.au>
2024-06-02 11:50:42 +1000

    Round configuration screen is done

--- screens/RoundConfiguration.js
+++ screens/RoundConfiguration.js
@@ -14,7 +14,7 @@ import {
   Pressable,
   HStack,
   ScrollView,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
 } from "native-base";
 import { useState, useEffect } from "react";
 import Background from "../components/Background";
@@ -74,14 +74,20 @@ const RoundConfigurationScreen = ({ navigation }) => {
     };
     updateRound(roundData, userData.token);
   };
-  const handleUpload = () => {
+  const handleValidateUpload = () => {
     let newIsInvalid = { ...isInvalid };
-
+// Validate level
     if (!level) {
       newIsInvalid.level = true;
     } else {
       newIsInvalid.level = false;
     }
+    // Validate roundName
+  if (!roundName) {
+    newIsInvalid.roundName = true;
+  } else {
+    newIsInvalid.roundName = false;
+  }
 
     setIsInvalid(newIsInvalid);
 
@@ -97,16 +103,16 @@ const RoundConfigurationScreen = ({ navigation }) => {
 
   return (
     <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-  >
-    <Center w="100%">
-      <ScrollView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Center w="100%">
+        {/* <ScrollView
         w="100%"
         h="100%"
         keyboardShouldPersistTaps="handled"
         contentContainerStyle={{ flexGrow: 1, minHeight: "100%" }}
-      >
+      > */}
         <Background />
         <Box
           alignItems="center"
@@ -118,7 +124,7 @@ const RoundConfigurationScreen = ({ navigation }) => {
           maxW={320}
         >
           <Center w="90%" h="100%">
-             <VStack w="100%" h="100%" space={6}>
+            <VStack w="100%" h="100%" space={6}>
               {/*<VStack>
                 <Text
                   mt={10}
@@ -130,185 +136,194 @@ const RoundConfigurationScreen = ({ navigation }) => {
                 </Text>
               </VStack> */}
               <VStack w="100%" space={6}>
-               {/* Round Name */}
-        <FormControl isInvalid={isInvalid.roundName}>
-          <FormControl.Label
-            ml={1}
-            _text={{
-              fontFamily: "Regular Semi Bold",
-              fontSize: "lg",
-              color: "#191919",
-            }}
-          >
-            Round Name
-          </FormControl.Label>
-          <Input
-          borderColor="#49a579"
-                  rounded="30"
-                  fontFamily={"Regular Medium"}
-                  size="lg"
-                  mr={3}
-                  w="93%"
-            placeholder="Enter Round Name"
-            value={roundName}
-            onChangeText={setRoundName}
-          />
-        </FormControl>
-        {/* Level */}
-        <FormControl isInvalid={isInvalid.level}>
-          <FormControl.Label
-            ml={1}
-            _text={{
-              fontFamily: "Regular Semi Bold",
-              fontSize: "lg",
-              color: "#191919",
-            }}
-          >
-            Level
-          </FormControl.Label>
-        </FormControl>
-        {/* // Dropdown list */}
-        <DropDownPicker
-          // dropDownContainerStyle={dropDownContainer}
-          open={open}
-          value={value}
-          items={items}
-          setOpen={setOpen}
-          setValue={(callback) => {
-            setValue(callback);
-            const newValue = callback(value);
-            setLevel(newValue);
-          }}
-          setItems={setItems}
-          
-        />
-        {isInvalid && (
-          <FormControl isInvalid={isInvalid.level}>
-            <FormControl.ErrorMessage>
-              This field is required
-            </FormControl.ErrorMessage>
-          </FormControl>
-        )}
-        {/* Start Date */}
-        <FormControl>
-          <HStack
-            space={2}
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <VStack>
-              <FormControl.Label
-                ml={1}
-                _text={{
-                  fontFamily: "Regular Semi Bold",
-                  fontSize: "lg",
-                  color: "#191919",
-                }}
-              >
-                Start Date
-              </FormControl.Label>
-              <Text>
-                {startDate.toLocaleDateString(undefined, {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </Text>
-            </VStack>
-
-            <View style={{ padding: 20, alignSelf: "flex-end" }}>
-              <ZStack alignSelf="flex-end" mr="15%" mt="10%">
-                <Box alignSelf="flex-end">
-                  <Menu
-                    mt="-50%"
-                    // mr="10"
-                    w="280"
-                    trigger={(triggerProps) => {
-                      return (
-                        <Pressable
-                          accessibilityLabel="Date picker"
-                          {...triggerProps}
-                          //                         onPress={() => {
-                          //   console.log("Calendar icon pressed. Start Date is:", startDate); // Replace this with your actual function to show date picker
-                          // }}
-                        >
-                          <AntDesign
-                            name="calendar"
-                            size={24}
-                            color="black"
-                          />
-                        </Pressable>
-                      );
+                {/* Round Name */}
+                <FormControl isInvalid={isInvalid.roundName}>
+                  <FormControl.Label
+                    ml={1}
+                    _text={{
+                      fontFamily: "Regular Semi Bold",
+                      fontSize: "lg",
+                      color: "#191919",
                     }}
                   >
-                    <DateTimePicker
-                      mode="single"
-                      date={startDate}
-                      minDate={minDaysFromNow}
-                      // onChange={(params) => setDate(params.date)}
-                      onChange={(params) => {
-                        setDate(new Date(params.date));
-                        console.log(
-                          "Calendar icon pressed. Start Date is:",
-                          startDate
-                        );
-                      }}
-                    />
-                  </Menu>
-                </Box>
-              </ZStack>
-            </View>
-          </HStack>
-        </FormControl>
-        {/* Maximum Capacity */}
-        <FormControl>
-          <FormControl.Label
-            ml={1}
-            _text={{
-              fontFamily: "Regular Semi Bold",
-              fontSize: "lg",
-              color: "#191919",
-            }}
-          >
-            Maximum Capacity
-          </FormControl.Label>
-          <Input
-            placeholder="Enter Max Capacity"
-            keyboardType="numeric"
-            value={maxCapacity}
-            onChangeText={setMaxCapacity}
-            borderColor="#49a579"
-                  rounded="30"
-                  fontFamily={"Regular Medium"}
-                  size="lg"
-                  mr={3}
-                  w="93%"
-          />
-        </FormControl>
-        <FormControl>
-          <FormControl.Label
-            ml={1}
-            _text={{
-              fontFamily: "Regular Semi Bold",
-              fontSize: "lg",
-              color: "#191919",
-            }}
-          >
-            Allow others to invite friends
-          </FormControl.Label>
-          <Switch
-            value={isEnabled}
-            onValueChange={toggleSwitch}
-            color={isEnabled ? "green" : "gray"}
-          />
-        </FormControl>
+                    Round Name
+                  </FormControl.Label>
+                  <Input
+                    borderColor="#49a579"
+                    rounded="30"
+                    fontFamily={"Regular Medium"}
+                    size="lg"
+                    mr={3}
+                    w="93%"
+                    placeholder="Enter Round Name"
+                    value={roundName}
+                    onChangeText={setRoundName}
+                  />
+                  {isInvalid.roundName && (
+    <FormControl.ErrorMessage>
+      Round Name is required.
+    </FormControl.ErrorMessage>
+  )}
+                </FormControl>
+                {/* Level */}
+                <FormControl isInvalid={isInvalid.level}>
+                  <FormControl.Label
+                    ml={1}
+                    _text={{
+                      fontFamily: "Regular Semi Bold",
+                      fontSize: "lg",
+                      color: "#191919",
+                    }}
+                  >
+                    Level
+                  </FormControl.Label>
+                </FormControl>
+                {/* // Dropdown list */}
+                <DropDownPicker
+                  // dropDownContainerStyle={dropDownContainer}
+                  open={open}
+                  value={value}
+                  items={items}
+                  setOpen={setOpen}
+                  setValue={(callback) => {
+                    setValue(callback);
+                    const newValue = callback(value);
+                    setLevel(newValue);
+                  }}
+                  setItems={setItems}
+                />
+                {isInvalid.level && (
+                  <FormControl isInvalid={isInvalid.level}>
+                    <FormControl.ErrorMessage>
+                      This field is required
+                    </FormControl.ErrorMessage>
+                  </FormControl>
+                )}
+                {/* Start Date */}
+                <FormControl>
+                  <HStack
+                    space={2}
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <VStack>
+                      <FormControl.Label
+                        ml={1}
+                        _text={{
+                          fontFamily: "Regular Semi Bold",
+                          fontSize: "lg",
+                          color: "#191919",
+                        }}
+                      >
+                        Start Date
+                      </FormControl.Label>
+                      <Text  ml={1}
+                        _text={{
+                          fontFamily: "Regular Semi Bold",
+                          fontSize: "lg",
+                          color: "#191919",
+                        }}>
+                        {startDate.toLocaleDateString(undefined, {
+                          year: "numeric",
+                          month: "short",
+                          day: "numeric",
+                        })}
+                      </Text>
+                    </VStack>
 
-        <Button
-          onPress={() => {
-            handleUpload();
-            // console.log("Calendar icon pressed. info:", startDate,level,roundName,allowOthers,userData.data._id);
-          }}
-          mt="5"
+                    <View style={{ padding: 20, alignSelf: "flex-end" }}>
+                      <ZStack alignSelf="flex-end" mr="15%" mt="10%">
+                        <Box alignSelf="flex-end">
+                          <Menu
+                            mt="-50%"
+                            // mr="10"
+                            w="280"
+                            trigger={(triggerProps) => {
+                              return (
+                                <Pressable
+                                  accessibilityLabel="Date picker"
+                                  {...triggerProps}
+                                  //                         onPress={() => {
+                                  //   console.log("Calendar icon pressed. Start Date is:", startDate); // Replace this with your actual function to show date picker
+                                  // }}
+                                >
+                                  <AntDesign
+                                    name="calendar"
+                                    size={24}
+                                    color="black"
+                                  />
+                                </Pressable>
+                              );
+                            }}
+                          >
+                            <DateTimePicker
+                              mode="single"
+                              date={startDate}
+                              minDate={minDaysFromNow}
+                              // onChange={(params) => setDate(params.date)}
+                              onChange={(params) => {
+                                setDate(new Date(params.date));
+                                console.log(
+                                  "Calendar icon pressed. Start Date is:",
+                                  startDate
+                                );
+                              }}
+                            />
+                          </Menu>
+                        </Box>
+                      </ZStack>
+                    </View>
+                  </HStack>
+                </FormControl>
+                {/* Maximum Capacity */}
+                <FormControl>
+                  <FormControl.Label
+                    ml={1}
+                    _text={{
+                      fontFamily: "Regular Semi Bold",
+                      fontSize: "lg",
+                      color: "#191919",
+                    }}
+                  >
+                    Maximum Capacity
+                  </FormControl.Label>
+                  <Input
+                    placeholder="Enter Max Capacity"
+                    keyboardType="numeric"
+                    value={maxCapacity}
+                    onChangeText={setMaxCapacity}
+                    borderColor="#49a579"
+                    rounded="30"
+                    fontFamily={"Regular Medium"}
+                    size="lg"
+                    mr={3}
+                    w="93%"
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label
+                    ml={1}
+                    _text={{
+                      fontFamily: "Regular Semi Bold",
+                      fontSize: "lg",
+                      color: "#191919",
+                    }}
+                  >
+                    Allow others to invite friends
+                  </FormControl.Label>
+                  <Switch
+                    value={isEnabled}
+                    onValueChange={toggleSwitch}
+                    color={isEnabled ? "green" : "gray"}
+                  />
+                </FormControl>
+
+                <Button
+                  onPress={() => {
+                    handleValidateUpload();
+                    // console.log("Calendar icon pressed. info:", startDate,level,roundName,allowOthers,userData.data._id);
+                  }}
+                  mt="5"
                   width="100%"
                   size="lg"
                   bg="#49a579"
@@ -317,20 +332,17 @@ const RoundConfigurationScreen = ({ navigation }) => {
                     fontFamily: "Regular Medium",
                     fontSize: "lg",
                   }}
-        >
-          Update Round
-        </Button>
-                
+                >
+                  Update Round
+                </Button>
               </VStack>
             </VStack>
           </Center>
         </Box>
-      </ScrollView>
-    </Center>
-  </KeyboardAvoidingView>
+        {/* </ScrollView> */}
+      </Center>
+    </KeyboardAvoidingView>
   );
 };
 
 export default RoundConfigurationScreen;
-
-//TODO: 2. Round name placeholder rather than default value
