3c451372abd3c2868d62d3ca5095711004940fda
Siya(Xiran) Yan <siyayan1222@gmail.com>
2024-06-12 18:42:01 +1000

    feat: forum draft page

--- screens/ForumDraft.js
+++ screens/ForumDraft.js
@@ -1,20 +1,46 @@
-import { Icon, Box, Text ,Center} from "native-base";
+import { Icon, Box, View, Text, Center, Image } from "native-base";
+import { TextareaItem, Button } from "@ant-design/react-native";
 
 import { useState, useEffect } from "react";
 import React, { useRef } from "react";
 import Background from "../components/Background";
 
-const ForumDraft = ({ navigation, route}) => {
-  const { result } = route.params;
+const ForumDraft = ({ navigation, route }) => {
+  const { res } = route.params;
   useEffect(() => {
-    console.log(result,"---------------")
+    console.log(res, "---------------");
   }, []);
   return (
     <Center w="100%">
-    <Background />
-    <Text>1111</Text>
+      <Background />
+      <Box height={"100%"} width={"100%"} alignItems={"center"}>
+        <View
+          style={{
+            height: "40%",
+          }}
+        >
+          <Image
+            
+            source={{
+              uri: res,
+            }}
+            alt="Alternate Text"
+            size="xl"
+          />
+        </View>
+        <View w="85%">
+          <TextareaItem
+            clear={true}
+            style={{ fontFamily: "Bold" }}
+            rows={10}
+            placeholder="Input yout text."
+            count={200}
+          />
+          {/* <Button></Button> */}
+          <Button type="primary">default</Button>
+        </View>
+      </Box>
     </Center>
-
   );
 };
 
