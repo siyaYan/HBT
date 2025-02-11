0bcd37f2274d9e8c21a2630466472dc8a9a8ef32
Siya(Xiran) Yan <siyayan1222@gmail.com>
2025-02-11 22:31:41 +1100

    fix:try hide keyboard hwen unfocus

--- screens/ForumDraft.js
+++ screens/ForumDraft.js
@@ -1,7 +1,8 @@
-import { Icon, Box, View, Text, Center, Image, Button } from "native-base";
+import { Box, View, Center, Image, Button } from "native-base";
 import { TextareaItem } from "@ant-design/react-native";
 import { useState, useEffect } from "react";
-import React, { useRef } from "react";
+import React from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import Background from "../components/Background";
 import { addPost } from "../components/Endpoint";
 import { useData } from "../context/DataContext";
@@ -33,62 +34,65 @@ const ForumDraft = ({ navigation, route }) => {
       });
     }
   };
+
   return (
-    <Center w="100%">
-      <Background />
-      <Box height={"100%"} width={"100%"} alignItems={"center"}>
-        <View w="85%" h={"32%"} marginTop={"10%"} alignItems={"center"}>
-          <Image
-            borderRadius={10}
-            source={{
-              uri: res.uri,
-            }}
-            alt="Alternate Text"
-            width={"100%"}
-            height={"100%"}
-            resizeMode="cover"
-          />
-        </View>
-        <View w="85%" h={"48%"} margin={"10%"}>
-          <TextareaItem
-            onChange={(value) => {
-              setPost({ ...post, text: value });
-            }}
-            borderRadius={10}
-            clear
-            style={{
-              lineHeight: 25,
-              fontSize: 20,
-              fontFamily: "Regular Medium",
-            }}
-            rows={10}
-            placeholder="Input yout text."
-            count={150}
-          />
-          <Button
-            onPress={handlePost}
-            mt="15%"
-            width="100%"
-            size="lg"
-            bg="#49a579"
-            _text={{
-              color: "#f9f8f2",
-              fontFamily: "Regular Medium",
-              fontSize: "lg",
-            }}
-            _pressed={{
-              // below props will only be applied on button is pressed
-              bg: "emerald.600",
-              _text: {
-                color: "warmGray.50",
-              },
-            }}
-          >
-            Post
-          </Button>
-        </View>
-      </Box>
-    </Center>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <Center w="100%">
+        <Background />
+        <Box height={"100%"} width={"100%"} alignItems={"center"}>
+          <View w="85%" h={"32%"} marginTop={"10%"} alignItems={"center"}>
+            <Image
+              borderRadius={10}
+              source={{
+                uri: res.uri,
+              }}
+              alt="Alternate Text"
+              width={"100%"}
+              height={"100%"}
+              resizeMode="cover"
+            />
+          </View>
+          <View w="85%" h={"48%"} margin={"10%"}>
+            <TextareaItem
+              onChange={(value) => {
+                setPost({ ...post, text: value });
+              }}
+              borderRadius={10}
+              clear
+              style={{
+                lineHeight: 25,
+                fontSize: 20,
+                fontFamily: "Regular Medium",
+              }}
+              rows={10}
+              placeholder="Input yout text."
+              count={150}
+            />
+            <Button
+              onPress={handlePost}
+              mt="15%"
+              width="100%"
+              size="lg"
+              bg="#49a579"
+              _text={{
+                color: "#f9f8f2",
+                fontFamily: "Regular Medium",
+                fontSize: "lg",
+              }}
+              _pressed={{
+                // below props will only be applied on button is pressed
+                bg: "emerald.600",
+                _text: {
+                  color: "warmGray.50",
+                },
+              }}
+            >
+              Post
+            </Button>
+          </View>
+        </Box>
+      </Center>
+    </TouchableWithoutFeedback>
   );
 };
 
